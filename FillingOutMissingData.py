b34323518c78d0a2e295d32a9c5bd7fe48e4168f
SkipClassGoGym <nguyenquocthinh1122@gmail.com>
2025-09-28 08:42:44 +0700

    DataPreprocessing

---
+++ FillingOutMissingData.py
@@ -0,0 +1,17 @@
+from numpy import nan as NA
+import pandas as pd 
+
+data = pd.DataFrame([[1., 6.5, 3.],
+                     [1., NA, NA],
+                     [NA, NA, NA],
+                     [NA, 6.5, 3.]])
+
+print(data)
+print('-'*50)
+cleaned = data.dropna()
+print(cleaned)
+print('-'*50)
+
+cleaned2 = data.dropna(how = 'all')
+print(cleaned2)
+
